b67d5cefd9fd925ce43c5e6435d52ea14ede593c
Pablo <pablo@Pablos-MBP.fibertel.com.ar>
2022-07-16 21:58:43 -0300

    Complete and delete logic completed.

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -2,17 +2,11 @@ import React from "react";
 import '../Styles/TodoItem.css';
 
 function TodoItem(props) {
-    const onComplete = () => {
-        alert('Completaste ' + props.text)
-    }
-    const onDelete = () => {
-        alert('Borraste ' + props.text)
-    }
     return (
         <li className="TodoItem">
             <span 
                 className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-                onClick={() => onComplete()}
+                onClick={props.onComplete}
             >
                 √
             </span>
@@ -21,7 +15,7 @@ function TodoItem(props) {
             </p>
             <span 
                 className="Icon Icon-delete"
-                onClick={ () => onDelete()}
+                onClick={ props.onDelete}
             >
                 X
             </span>
